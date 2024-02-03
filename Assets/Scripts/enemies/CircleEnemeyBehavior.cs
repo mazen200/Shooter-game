db9c3965b0df0fc65624eec5eea3d2e89e09cb4f
mazen <mazen15408@gmail.com>
2024-02-04 01:22:51 +0200

    add death animation to first enemy

--- Assets/Scripts/enemies/CircleEnemeyBehavior.cs
+++ Assets/Scripts/enemies/CircleEnemeyBehavior.cs
@@ -8,20 +8,40 @@ public class CircleEnemeyBehavior : MonoBehaviour
     Vector3 dir;
     Transform player;
     Rigidbody2D rb;
+    Animator animator;
     private void Start()
     {
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         findDirctionPlayer();
-        setSpeed();
+        setSpeed(dir.normalized * speed);
     }
-
+    private void Update()
+    {
+        if(animator.GetBool("dead"))
+            setSpeed(new Vector2(0.0f,0.0f));
+    }
     void findDirctionPlayer()
     {
         player = GameObject.FindGameObjectWithTag("Player").transform;
         dir = player.transform.position - transform.position;
     }
-    void setSpeed()
+    void setSpeed(Vector2 speed)
     {
-        rb.velocity = dir.normalized * speed;
+        rb.velocity = speed;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("PlayerBullet"))
+        {
+            animator.SetBool("dead", true);
+            StartCoroutine("remove");
+        }
+    }
+
+    IEnumerator remove()
+    {
+        yield return new WaitForSeconds(1f);
+        Destroy(gameObject);
     }
 }
